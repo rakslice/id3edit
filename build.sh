b9da5995e0974303f7ea1f924651b4e068499e03
rakslice <rakslice@gmail.com>
2022-08-21 18:32:40 -0700

    build script changes for mingw32; show compile commands

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 SOURCE=$(find . -type f ! -path './test/*' -name "*.c")
-HEADER="-I."
-LIBS="-lprinthex -lz"
+HEADER="-I. -I../libprinthex"
+LIBS="-L../libprinthex -lprinthex -lz -lWs2_32 -liconv"
+CC=gcc
 
 for c in $SOURCE ;
 do    
     echo -e "\e[1;34mCompiling $c …\e[0m"
-    clang -DxDEBUG -g -Wno-multichar --std=gnu99 $HEADER -O2 -g -c -o "${c%.*}.o" $c
+    ( set -x ; $CC -DxDEBUG -g -Wno-multichar --std=gnu99 $HEADER -O2 -g -c -o "${c%.*}.o" $c )
 done
 
 
 OBJECTS=$(find . -type f ! -path './test/*' -name "*.o")
 
-clang -o id3edit $OBJECTS $LIBS
+( set -x ; $CC -o id3edit $OBJECTS $LIBS )
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
